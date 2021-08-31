1c1dddf08279db4746647bb13e59bd7d21e66f79
Irene Benedetto <irenebenedetto@LHs-iPad-mini-2.localdomain>
2021-08-31 23:37:07 +0200

    added model

---
+++ sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,34 @@
+from transformers import pipeline
+import streamlit as st
+
+
+def app():
+    st.title('Sentiment analysis')
+    st.write("Analyse a small sentence and understand whether is positive, negative, or neutral.")
+
+    default_text = """
+        I really like the new design of your website!
+        """.strip()
+    text = st.text_area('Enter a text to analyze', default_text)
+
+    button = st.button('Analyze the sentence')
+    if button:
+        sentiment_analysis = pipeline("sentiment-analysis", model="./sentiment_analysis/checkpoints/model/")
+        sentiment = sentiment_analysis(text)[0]
+        
+
+
+        if sentiment['label'] == 'POSITIVE' or sentiment['label'] == 'LABEL_1':
+            col1, col2 = st.columns((14, 1))
+            col2.image('./sentiment_analysis/img/up.png')
+            col1.subheader(f'Sentiment POSITIVE ({round(sentiment["score"] * 100, 2)}%)\n')
+        elif sentiment['label'] == 'NEGATIVE' or sentiment['label'] == 'LABEL_0':
+            col1, col2 = st.columns((14, 1))
+            col2.image('./sentiment_analysis/img/down.png')
+            col1.subheader(f'Sentiment NEGATIVE ({round(sentiment["score"] * 100, 2)}%)\n')
+        else:
+            col1, col2 = st.columns((14, 1))
+            col2.image('./sentiment_analysis/img/neu.png')
+            col1.subheader(f'Sentiment {sentiment["label"]} ({round(sentiment["score"] * 100, 2)}%)\n')
+
+
